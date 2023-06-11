2a1c96626cd083e212b3951773084795f330fd75
NaboKabbo <101510622+PythonChicken123@users.noreply.github.com>
2023-06-11 20:04:16 +0100

    Add files via upload

---
+++ lib/upload.sh
@@ -0,0 +1,6 @@
+# Adjust based on your needs
+
+# cp setup.py /media/alwin/CIRCUITPY/code.py
+
+cp keyboard_layout_win_de.py /media/alwin/JUST_USB/keyboard_layout_win_de.py
+cp keyboard.py /media/alwin/HackyPi/code.py
